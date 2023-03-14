3223d9785186130c122873342f75476c78bee650
envestcc <chen1233216@hotmail.com>
2023-03-15 00:29:11 +0800

    add test for owner

--- test/SystemStaking.spec.ts
+++ test/SystemStaking.spec.ts
@@ -34,13 +34,194 @@ describe("SystemStaking", () => {
 
     before(async () => {
         ;[owner, staker, alice] = await ethers.getSigners()
+    })
 
-        const factory = await ethers.getContractFactory("SystemStaking")
-        system = (await factory.deploy()) as SystemStaking
+    describe("owner", () => {
+        before(async () => {
+            const factory = await ethers.getContractFactory("SystemStaking")
+            system = (await factory.deploy()) as SystemStaking
+        })
+
+        describe("pause", () => {
+            it("not owner pause", async () => {
+                await expect(
+                    system.connect(staker).pause()
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+    
+            it("multiple pause", async () => {
+                await system.connect(owner).pause()
+                await expect(
+                    system.connect(owner).pause()
+                ).to.be.revertedWith("Pausable: paused")
+            })
+    
+            it("not owner unpause", async () => {
+                await expect(
+                    system.connect(staker).unpause()
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+    
+            it("multiple unpause", async () => {
+                await system.connect(owner).unpause()
+                await expect(
+                    system.connect(owner).unpause()
+                ).to.be.revertedWith("Pausable: not paused")
+            })
+        })
+
+        describe("emergency withdraw panalty rate", () => {
+            it("not owner set", async () => {
+                await expect(
+                    system.connect(staker).setEmergencyWithdrawPenaltyRate(90)
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+
+            it("invalid penalty rate", async () => {
+                await expect(
+                    system.connect(owner).setEmergencyWithdrawPenaltyRate(110)
+                ).to.be.revertedWith("invaid penalty rate")
+            })
+
+            it("not owner read", async () => {
+                await system.connect(owner).setEmergencyWithdrawPenaltyRate(80)
+                await expect(
+                    await system.connect(staker).emergencyWithdrawPenaltyRate()
+                ).to.equal(80)
+            })
+        })
+
+        describe("bucket type", () => {
+            let system2: SystemStaking
+
+            beforeEach(async () => {
+                const factory = await ethers.getContractFactory("SystemStaking")
+                system2 = (await factory.deploy()) as SystemStaking
+            })
+
+            it("not owner add", async () => {
+                await expect(
+                    system2.connect(staker).addBucketType(100, ONE_DAY)
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+
+            it("add zero amount", async () => {
+                await expect(
+                    system2.connect(owner).addBucketType(0, ONE_DAY)
+                ).to.be.revertedWith("amount is invalid")
+            })
+            
+            it("add success", async () => {
+                await system2.connect(owner).addBucketType(10, ONE_DAY)
+                await expect(
+                    await system2.connect(owner).isActiveBucketType(10, ONE_DAY)
+                ).to.equal(true)
+                await expect(
+                    await system2.connect(owner).numOfBucketTypes()
+                ).to.equal(1)
+                let types = await system2.connect(owner).bucketTypes(0, 1)
+                expect(types.length).to.equal(1)
+                expect(types[0].amount).to.equal(10)
+                expect(types[0].duration).to.equal(ONE_DAY)
+                expect(types[0].activatedAt).to.equal(await ethers.provider.getBlockNumber())
+            })
+
+            it("add duplicate", async () => {
+                await system2.connect(owner).addBucketType(10, ONE_DAY)
+                await expect(
+                    system2.connect(owner).addBucketType(10, ONE_DAY)
+                ).to.be.revertedWith("duplicate bucket type")
+            })
+
+            it("add multiple", async () => {
+                await system2.connect(owner).addBucketType(10, ONE_DAY)
+                await system2.connect(owner).addBucketType(20, ONE_DAY)
+                await system2.connect(owner).addBucketType(30, ONE_DAY)
+                expect(await system2.connect(owner).numOfBucketTypes()).to.equal(3)
+            })
+
+            it("not owner deactivate", async () => {
+                await system2.connect(owner).addBucketType(100, ONE_DAY)
+                await expect(
+                    system2.connect(staker).deactivateBucketType(100, ONE_DAY)
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+
+            it("deactivate invalid", async () => {
+                await expect(
+                    system2.connect(owner).deactivateBucketType(100, ONE_DAY)
+                ).to.be.revertedWith("invalid bucket type")
+            })
+
+            it("deactivate success", async () => {
+                await system2.connect(owner).addBucketType(100, ONE_DAY)
+                await system2.connect(owner).deactivateBucketType(100, ONE_DAY)
+                await expect(
+                    await system2.connect(owner).isActiveBucketType(100, ONE_DAY)
+                ).to.equal(false)
+            })
+
+            it("not owner activate", async () => {
+                await system2.connect(owner).addBucketType(100, ONE_DAY)
+                await system2.connect(owner).deactivateBucketType(100, ONE_DAY)
+                await expect(
+                    system2.connect(staker).activateBucketType(100, ONE_DAY)
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+
+            it("activate invalid", async () => {
+                await expect(
+                    system2.connect(owner).activateBucketType(100, ONE_DAY)
+                ).to.be.revertedWith("invalid bucket type")
+            })
+
+            it("activate success", async () => {
+                await system2.connect(owner).addBucketType(100, ONE_DAY)
+                await system2.connect(owner).deactivateBucketType(100, ONE_DAY)
+                await system2.connect(owner).activateBucketType(100, ONE_DAY)
+                await expect(
+                    await system2.connect(owner).isActiveBucketType(100, ONE_DAY)
+                ).to.equal(true)
+            })
+        })
+
+        describe("withdraw fee", () => {
+            it("not owner", async () => {
+                await expect(
+                    system.connect(staker).withdrawFee(0, staker.address)
+                ).to.be.revertedWith("Ownable: caller is not the owner")
+            })
+
+            it("invalid amount", async () => {
+                await expect(
+                    system.connect(owner).withdrawFee(100, staker.address)
+                ).to.be.revertedWith("invalid amount")
+            })
+
+            it("success", async () => {
+                await system.connect(owner).setEmergencyWithdrawPenaltyRate(10)
+                await system.connect(owner).addBucketType(100, ONE_DAY)
+                let tokenId = await createBucket(
+                    system, 
+                    staker, 
+                    ONE_DAY, 
+                    100,
+                    ethers.utils.hexlify(ethers.utils.toUtf8Bytes("123456789012")),
+                )
+                await system.connect(staker).emergencyWithdraw(tokenId, staker.address)
+                expect(await system.connect(staker).accumulatedWithdrawFee()).to.equal(10)
+                let oldBalance = await alice.getBalance()
+                await system.connect(owner).withdrawFee(5, alice.address)
+                expect(await alice.getBalance()).to.equal(ethers.BigNumber.from(5).add(oldBalance))
+                expect(await system.connect(owner).accumulatedWithdrawFee()).to.equal(5)
+            })
+        })
     })
 
     describe("flow", () => {
         before(async () => {
+            const factory = await ethers.getContractFactory("SystemStaking")
+            system = (await factory.deploy()) as SystemStaking
             await system.connect(owner).addBucketType(ethers.utils.parseEther("1"), ONE_DAY)
         })
 
